0323dfa8cd7c4f3a877b2ec9f9652a1610347dee
RundesBalli <GitHub@RundesBalli.com>
2022-06-12 01:45:54 +0200

    CSS ausgedünnt und unnötige Mediaqueries entfernt

--- public/inc/403.php
+++ public/inc/403.php
@@ -9,6 +9,6 @@ $title = "403";
 http_response_code(403);
 $content.= "<h1>403 - Forbidden</h1>".PHP_EOL;
 $content.= "<div class='row'>".PHP_EOL.
-"<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'>Du hast keine Berechtigung auf die von dir angeforderte Ressource <span class='italic'>".htmlentities($_SERVER['REQUEST_URI'], ENT_QUOTES)."</span> zuzugreifen.</div>".PHP_EOL.
+"<div class='col-s-12 col-l-12'>Du hast keine Berechtigung auf die von dir angeforderte Ressource <span class='italic'>".htmlentities($_SERVER['REQUEST_URI'], ENT_QUOTES)."</span> zuzugreifen.</div>".PHP_EOL.
 "</div>".PHP_EOL;
 ?>
