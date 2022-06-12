2a1780c8af5ae9903f9451ba5bf8556c1e57b8a0
RundesBalli <GitHub@RundesBalli.com>
2022-06-12 02:25:58 +0200

    Text zentriert

--- public/inc/all.php
+++ public/inc/all.php
@@ -9,7 +9,7 @@ $content.= "<h1>Alle Einträge</h1>";
 $content.= "<div class='row'>";
 $result = mysqli_query($dbl, "SELECT * FROM `items` ORDER BY `title` ASC") OR DIE(MYSQLI_ERROR($dbl));
 while($row = mysqli_fetch_array($result)) {
-  $content.= "<div class='col-s-12 col-l-4 item hover'><a href='".$row['url']."' target='_blank' rel='noopener'>".$row['title']."</a></div>";
+  $content.= "<div class='col-s-12 col-l-4 item hover center'><a href='".$row['url']."' target='_blank' rel='noopener'>".$row['title']."</a></div>";
 }
 $content.= "</div>";
 ?>
