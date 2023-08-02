7123c060aa9ac0c1e15ce8f9487dd32a5f8e4756
RundesBalli <GitHub@RundesBalli.com>
2023-08-02 12:17:08 +0200

    Old stuff removed. Made space for V3

--- includes/sql.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * sql.php
- * 
- * Stellt die Datenbankverbindung her und richtet das korrekte Charset ein.
- */
-$dbl = mysqli_connect($mysqlHost, $mysqlUser, $mysqlPass, $mysqlDb) OR DIE(MYSQLI_ERROR($dbl));
-mysqli_set_charset($dbl, $mysqlCharset) OR DIE(MYSQLI_ERROR($dbl));
-?>
