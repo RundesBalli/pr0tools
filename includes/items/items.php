56ce8474f1b375b2b9ef3af44c0829d6ae7e636b
RundesBalli <GitHub@RundesBalli.com>
2024-12-29 18:59:36 +0100

    Typos

--- includes/items/items.php
+++ includes/items/items.php
@@ -21,37 +21,37 @@ $ci = [
         'thumb' => 'eckdaten',
         'name' => 'Eckdaten',
         'author' => 'holzmaster',
-        'description' => 'Eckdaten, Farben, Bilder für Textposts'
+        'description' => 'Eckdaten, Farben, Bilder für Textposts',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3295515',
         'thumb' => 'thumbnails',
         'name' => 'Thumbnail mit Rahmen',
         'author' => 'Retrowinger',
-        'description' => 'Anleitung für schöne Thumbnails mit Rahmen'
+        'description' => 'Anleitung für schöne Thumbnails mit Rahmen',
       ],
       [
         'url' => 'https://retropr0.github.io/pr0p0st/html/',
         'thumb' => 'pr0p0st',
         'name' => 'pr0p0st',
         'author' => 'retro',
-        'description' => 'Tool zur Erstellung von Textposts'
+        'description' => 'Tool zur Erstellung von Textposts',
       ],
       [
         'url' => 'https://pr0gramm.com/new/1947058',
         'thumb' => 'tts',
         'name' => 'text2speech Tutorial',
         'author' => 'qbl',
-        'description' => 'Text2Speech / Computerstimme für OC-Videos'
+        'description' => 'Text2Speech / Computerstimme für OC-Videos',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3156775',
         'thumb' => 'scribus',
         'name' => 'Scribus Tutorial',
         'author' => 'Retrowinger',
-        'description' => 'Anleitung zum Erstellen von Posts mittels Scribus'
+        'description' => 'Anleitung zum Erstellen von Posts mittels Scribus',
       ],
-    ]
+    ],
   ],
   'upload' => [
     'title' => 'Hochladen',
@@ -64,22 +64,23 @@ $ci = [
         'name' => 'pr0verter (derzeit offline!)',
         'author' => 'Insax',
         'description' => 'Konvertiere deine Videos direkt passend fürs pr0!'
+        'description' => 'Konvertiere deine Videos direkt passend fürs pr0!',
       ],
       [
         'url' => 'https://rep0st.rene8888.at/',
         'thumb' => 'rep0st',
         'name' => 'rep0st',
         'author' => 'Rene8888',
-        'description' => 'Repost Suche'
+        'description' => 'Repost Suche',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3322321',
         'thumb' => 'pr0verter_offline',
         'name' => 'pr0verter Offline',
         'author' => 'Pennypacker',
-        'description' => 'Der pr0verter als Offline-Version'
+        'description' => 'Der pr0verter als Offline-Version',
       ],
-    ]
+    ],
   ],
   'official' => [
     'title' => 'Offizielles',
@@ -91,58 +92,58 @@ $ci = [
         'thumb' => 'app',
         'name' => 'pr0gramm Android App',
         'author' => 'Mopsalarm',
-        'description' => 'Die offizielle pr0gramm Android App'
+        'description' => 'Die offizielle pr0gramm Android App',
       ],
       [
         'url' => 'https://pr0mart.com',
         'thumb' => 'pr0mart',
         'name' => 'pr0mart',
         'author' => 'pr0mart',
-        'description' => 'Der offizielle pr0gramm Merchandise Shop'
+        'description' => 'Der offizielle pr0gramm Merchandise Shop',
       ],
       [
         'url' => 'https://z0cken.com/',
         'thumb' => 'z0cken',
         'name' => 'z0cken.com',
         'author' => 'z0cken',
-        'description' => 'Multiplayer Projekt von pr0gramm'
+        'description' => 'Multiplayer Projekt von pr0gramm',
       ],
       [
         'url' => 'https://discord.gg/pr0',
         'thumb' => 'discord',
         'name' => 'Discord',
         'author' => 'pr0gramm',
-        'description' => 'Der offizielle pr0gramm Discordserver'
+        'description' => 'Der offizielle pr0gramm Discordserver',
       ],
       [
         'url' => 'https://discord.gg/mpJ6YYGzBq',
         'thumb' => 'pr0grammierer',
         'name' => 'pr0grammierer Discord',
         'author' => 'pr0gramm',
-        'description' => 'Der offizielle pr0gramm Entwickler Discordserver'
+        'description' => 'Der offizielle pr0gramm Entwickler Discordserver',
       ],
       [
         'url' => 'https://pr0gramm.com/search',
         'thumb' => 'erweiterteSuche',
         'name' => 'Erweiterte Suche',
         'author' => 'pr0gramm',
-        'description' => 'Die erweiterte Suche für noch bessere Suchergebnisse'
+        'description' => 'Die erweiterte Suche für noch bessere Suchergebnisse',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3323910',
         'thumb' => 'rueckwaertssuche',
         'name' => 'Rückwärtssuche',
         'author' => 'pr0gramm',
-        'description' => 'Der Infopost zur Rückwärtssuche auf pr0gramm'
+        'description' => 'Der Infopost zur Rückwärtssuche auf pr0gramm',
       ],
       [
         'url' => 'https://github.com/pr0gramm-com/api-docs',
         'thumb' => 'apiDocs',
         'name' => 'API-Dokumentation',
         'author' => 'pr0gramm',
-        'description' => 'API Dokumentation für Entwickler'
+        'description' => 'API Dokumentation für Entwickler',
       ],
-    ]
+    ],
   ],
   'scripts' => [
     'title' => 'Scripts & Addons',
@@ -154,44 +155,44 @@ $ci = [
         'thumb' => 'userscripts',
         'name' => 'Userscripts',
         'author' => 'holzmaster',
-        'description' => "Userscripts und Tools. Was es gibt, was gewollt wird."
+        'description' => 'Userscripts und Tools. Was es gibt, was gewollt wird.',
       ],
       [
         'url' => 'https://scarwolf.github.io/p0weruser/p0weruser.user.js',
         'thumb' => 'p0weruser',
         'name' => 'p0weruser Userscript',
         'author' => 'PoTTii',
-        'description' => 'Root-Zugriff auf das pr0gramm! WideScreen Modus, Notification Center, eingefärbte Kommentar-Ebenen, uvm.'
+        'description' => 'Root-Zugriff auf das pr0gramm! WideScreen Modus, Notification Center, eingefärbte Kommentar-Ebenen, uvm.',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3145896',
         'thumb' => 'fitImages',
         'name' => 'Bilder Scaling Userscript',
         'author' => 'findus',
-        'description' => 'Passt auf Tastendruck die Höhe des angezeigten Bildes an und macht es so möglich erwas höhere Bilder auf kleineren Monitoren zu anzuschauen ohne zu scrollen'
+        'description' => 'Passt auf Tastendruck die Höhe des angezeigten Bildes an und macht es so möglich erwas höhere Bilder auf kleineren Monitoren zu anzuschauen ohne zu scrollen',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3568968',
         'thumb' => 'sch0ngesehen',
         'name' => 'sch0ngesehen',
         'author' => 'Mwni',
-        'description' => 'Ein Browseraddon um anzuzeigen, welche Posts man bereits gesehen hat'
+        'description' => 'Ein Browseraddon um anzuzeigen, welche Posts man bereits gesehen hat',
       ],
       [
         'url' => 'https://addons.mozilla.org/de/firefox/collections/17356040/pr0-Themes/',
         'thumb' => 'themes',
         'name' => 'Themes für Firefox',
         'author' => 'J4it',
-        'description' => 'Firefox Themes in den bewährten pr0gramm Farben'
+        'description' => 'Firefox Themes in den bewährten pr0gramm Farben',
       ],
       [
         'url' => 'https://github.com/pr0-dev/pr0-tv',
         'thumb' => 'pr0tv',
         'name' => 'pr0-TV',
         'author' => 'TheShad0w',
-        'description' => 'Das Userscript bringt pr0-TV als Feature zurück!'
+        'description' => 'Das Userscript bringt pr0-TV als Feature zurück!',
       ],
-    ]
+    ],
   ],
   'community' => [
     'title' => 'Communityprojekte',
@@ -203,16 +204,16 @@ $ci = [
         'thumb' => 'kellertreff',
         'name' => 'Kellertreff',
         'author' => 'holzmaster',
-        'description' => 'Du suchst einen anderen Menschen? Zum Zocken, Unterhalten, für ein Projekt oder ein Treffen? Dann bist du beim Kellertreff genau richtig!'
+        'description' => 'Du suchst einen anderen Menschen? Zum Zocken, Unterhalten, für ein Projekt oder ein Treffen? Dann bist du beim Kellertreff genau richtig!',
       ],
       [
         'url' => 'http://badewannencalculator.de/',
         'thumb' => 'badewannencalculator',
         'name' => 'BadewannenCalculator',
         'author' => 'ufop',
-        'description' => 'Der praktische Rechner zum Umrechnen gemäß pr0-Norm 1'
+        'description' => 'Der praktische Rechner zum Umrechnen gemäß pr0-Norm 1',
       ],
-    ]
+    ],
   ],
   'bots' => [
     'title' => 'Nutzer-Bots',
@@ -224,30 +225,30 @@ $ci = [
         'thumb' => 'vergissmichnicht',
         'name' => '@vergissmichnicht',
         'author' => 'AlfonsoHater',
-        'description' => 'Erinnerungsbot für automatische Erinnerungen per PN'
+        'description' => 'Erinnerungsbot für automatische Erinnerungen per PN',
       ],
       [
         'url' => 'https://pr0gramm.com/new/4625113',
         'thumb' => 'sauce',
         'name' => '@Sauce',
         'author' => 'Helix',
-        'description' => 'Erkennt Musik in Videos und postet Titel und Interpret'
+        'description' => 'Erkennt Musik in Videos und postet Titel und Interpret',
       ],
       [
         'url' => 'https://pr0gramm.com/new/5783149',
         'thumb' => 'tldr',
         'name' => '@tldr',
         'author' => 'PimmelmannJones',
-        'description' => 'Zusammenfassung eines vorstehenden Textes'
+        'description' => 'Zusammenfassung eines vorstehenden Textes',
       ],
       [
         'url' => 'https://pr0gramm.com/new/5784369',
         'thumb' => 'linkers',
         'name' => '@linkers',
         'author' => 'Kavatch',
-        'description' => 'Entfernung lästiger Tracking Parameter in Links'
+        'description' => 'Entfernung lästiger Tracking Parameter in Links',
       ],
-    ]
+    ],
   ],
   'stats' => [
     'title' => 'Statistiken',
@@ -259,16 +260,16 @@ $ci = [
         'thumb' => 'w0chenstatistik',
         'name' => 'w0chenstatistik',
         'author' => 'DerpyDerp',
-        'description' => "Die Wochenstatistik.\nImmer Montags um 21:00 Uhr."
+        'description' => "Die Wochenstatistik.\nImmer Montags um 21:00 Uhr.",
       ],
       [
         'url' => 'https://pr0stats.github.io/',
         'thumb' => 'pr0stats',
         'name' => 'pr0stats (veraltet)',
         'author' => 'pr0stats',
-        'description' => 'Alte Statistiken über das pr0gramm.'
+        'description' => 'Alte Statistiken über das pr0gramm.',
       ],
-    ]
+    ],
   ],
   'niceToKnow' => [
     'title' => 'Gut zu wissen',
@@ -280,51 +281,51 @@ $ci = [
         'thumb' => 'badewanne',
         'name' => 'pr0-Norm #1 - Die Badewanne',
         'author' => 'J0unz',
-        'description' => 'Die allseits beliebte Badewanne genormt.'
+        'description' => 'Die allseits beliebte Badewanne genormt.',
       ],
       [
         'url' => 'https://pr0gramm.com/new/4239894',
         'thumb' => 'badewanne_1-2',
         'name' => 'pr0-Norm #1.2 - Aktualisierung',
         'author' => 'DerAckrige',
-        'description' => 'Eine erneute Aktualisierung der Badenwannennorm.'
+        'description' => 'Eine erneute Aktualisierung der Badenwannennorm.',
       ],
       [
         'url' => 'https://pr0gramm.com/new/4372488:comment45601148',
         'thumb' => '1typ',
         'name' => '1-Typ Stickerpack',
         'author' => 'Fehlgeburt',
-        'description' => 'Die beliebten 1Typ Bilder als Stickerpack für Telegram, WhatsApp & Signal. (Siehe Kommentarbaum)'
+        'description' => 'Die beliebten 1Typ Bilder als Stickerpack für Telegram, WhatsApp & Signal. (Siehe Kommentarbaum)',
       ],
       [
         'url' => 'https://pr0badges.github.io/',
         'thumb' => 'pr0badges',
         'name' => 'Badges',
         'author' => 'Blood32',
-        'description' => 'Übersicht aller Badges auf pr0gramm.'
+        'description' => 'Übersicht aller Badges auf pr0gramm.',
       ],
       [
         'url' => 'https://pr0gramm.com/new/1500962',
         'thumb' => 'gangsign',
         'name' => 'Gangsign 2.0',
         'author' => 'l00tSpastille',
-        'description' => 'Das pr0-Gangsign wie es sein sollte.'
+        'description' => 'Das pr0-Gangsign wie es sein sollte.',
       ],
       [
         'url' => 'https://pr0gramm.com/new/1579819',
         'thumb' => 'screenshots',
         'name' => 'große, lange Screenshots',
         'author' => 'Donnerstaender',
-        'description' => 'Tutorial um große, lange Screenshots zu erstellen.'
+        'description' => 'Tutorial um große, lange Screenshots zu erstellen.',
       ],
       [
         'url' => 'https://pr0gramm.com/new/1755191',
         'thumb' => 'tierduden',
         'name' => 'Tierduden v4',
         'author' => 'Unbefriedigend',
-        'description' => 'Der pr0gramm Tierduden ist eine Aufstellung aller Kadsen, Kefer und .EXEn'
+        'description' => 'Der pr0gramm Tierduden ist eine Aufstellung aller Kadsen, Kefer und .EXEn',
       ],
-    ]
+    ],
   ],
   'games' => [
     'title' => 'Spiele',
@@ -336,51 +337,51 @@ $ci = [
         'thumb' => 'z0cken',
         'name' => 'z0cken.com',
         'author' => 'z0cken',
-        'description' => 'Multiplayer Projekt von pr0gramm'
+        'description' => 'Multiplayer Projekt von pr0gramm',
       ],
       [
         'url' => 'https://pr0game.com/',
         'thumb' => 'pr0game',
         'name' => 'pr0game',
         'author' => 'Rosenreeman',
-        'description' => '"OGame" Klon ohne Premiumfunktionen'
+        'description' => '"OGame" Klon ohne Premiumfunktionen',
       ],
       [
         'url' => 'https://pr0gramm.com/new/2230627',
         'thumb' => 'flatterfogel',
         'name' => 'Flatterfogel',
         'author' => 'C0dingschmuser',
-        'description' => 'Ein Flappybird-Klon als pr0-Version'
+        'description' => 'Ein Flappybird-Klon als pr0-Version',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3877042',
         'thumb' => 'pr0ball-2_0',
         'name' => 'pr0ball 2.0',
         'author' => 'Mopsi91',
-        'description' => 'Sammelt Blussis, verzockt sie im Benitrat0r, verbessert eure Bälle und benutzt Skills um eure Gegner umzunieten'
+        'description' => 'Sammelt Blussis, verzockt sie im Benitrat0r, verbessert eure Bälle und benutzt Skills um eure Gegner umzunieten',
       ],
       [
         'url' => 'https://pr0gramm.com/new/2561200',
         'thumb' => 'pr0fense',
         'name' => 'Pr0fense',
         'author' => 'C0dingschmuser',
-        'description' => 'Tower-Defense Spiel im pr0gramm-Stil'
+        'description' => 'Tower-Defense Spiel im pr0gramm-Stil',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3839649',
         'thumb' => 'pr0rona',
         'name' => 'pr0rona',
         'author' => 'pr0nihilo',
-        'description' => 'Weiche geschickt den Viren aus, trinke Bier und erreiche deinen Hochwert'
+        'description' => 'Weiche geschickt den Viren aus, trinke Bier und erreiche deinen Hochwert',
       ],
       [
         'url' => 'https://pr0gramm.com/new/3126416',
         'thumb' => 'guess-the-tag',
         'name' => 'GuessTheTag',
         'author' => 'C0dingschmuser',
-        'description' => 'Tag-Ratespiel, bei dem man von einem Post den korrekten Tag erraten muss'
+        'description' => 'Tag-Ratespiel, bei dem man von einem Post den korrekten Tag erraten muss',
       ],
-    ]
-  ]
+    ],
+  ],
 ];
 ?>
